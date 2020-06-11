1468d710d77411b1ced604f0fbacd8b71318766c
MatviyO <33325458+MatviyO@users.noreply.github.com>
2020-06-11 23:49:59 +0300

    up

--- 4_mock/mock.spec.js
+++ 4_mock/mock.spec.js
@@ -2,11 +2,20 @@ const {map} = require('./mock')
 
 describe('Map function', () => {
     let array
+    let fn
 
     beforeEach(() => {
         array = [1,2,3,5]
+        fn = jest.fn(x => x ** 2)
     })
     test('should call callback', () => {
-        
+        map(array, fn)
+        expect(fn).toBeCalled()
+        expect(fn).toBeCalledTimes(4)
+
+    })
+    test('should call callback 4 times', () => {
+        map(array, fn)
+        expect(fn).toBeCalledTimes(4)
     })
 })
